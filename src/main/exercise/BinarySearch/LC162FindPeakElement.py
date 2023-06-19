dbc8dc87c88d281e10a7ba5b5f9796c9fae190f3
Chen Hua <chua0221@gmail.com>
2023-06-18 17:37:03 -0700

    add LC162 find peak element

---
+++ src/main/exercise/BinarySearch/LC162FindPeakElement.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums) <= 1: return 0
+        index = -1
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid-1] > nums[mid]: # peak found
+                index = mid - 1
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return len(nums) - 1 if index == -1 else index
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.findPeakElement([1,2,3,1]))
+    print(s.findPeakElement([1,2,1,3,5,6,4]))
+    print(s.findPeakElement([1,2]))
+    print(s.findPeakElement([2,1]))
+    print(s.findPeakElement([1]))
+    print(s.findPeakElement([6,5,4,3,2,3,2]))
+
