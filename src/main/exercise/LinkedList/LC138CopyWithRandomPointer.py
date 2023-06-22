b9f8b929bb23e60fe1ca9a211e4745927ad59d00
Chen Hua <chua0221@gmail.com>
2023-06-22 11:37:59 -0700

    add LC138 copy list with random pointer

---
+++ src/main/exercise/LinkedList/LC138CopyWithRandomPointer.py
@@ -0,0 +1,24 @@
+from model.Node import Node
+from typing import Optional
+
+
+class Solution:
+    def copyRandomList(self, head:'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        copy_map = {}
+
+        curr = head
+        while curr:
+            copied = Node(curr.val)
+            copy_map[curr] = copied
+            curr = curr.next
+
+        curr = head
+        while curr:
+            copied = copy_map.get(curr)
+            copied.next = copy_map.get(copied.next, None)
+            copied.random = copy_map.get(copied.random, None)
+            curr = curr.next
+
+        return copy_map.get(head)
