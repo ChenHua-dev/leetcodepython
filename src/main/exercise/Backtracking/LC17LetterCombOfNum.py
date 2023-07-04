35c6fcf6846c0e82f766f6af990f32c6077a31ff
Chen Hua <chua0221@gmail.com>
2023-07-03 21:29:27 -0700

    add LC17LetterCombOfNum.py

---
+++ src/main/exercise/Backtracking/LC17LetterCombOfNum.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.map = {"2": "abc",
+                    "3": "def",
+                    "4": "ghi",
+                    "5": "jkl",
+                    "6": "mno",
+                    "7": "pqrs",
+                    "8": "tuv",
+                    "9": "wxyz"}
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        if len(digits) <= 0:
+            return res
+        index = 0
+        substr = "" # start of the substring is ""
+        self.dfs(digits, res, index, substr)
+        return res
+
+    def dfs(self, digits: str, res: List[str], index: int, substr: str) -> None:
+        if index >= len(digits):
+            res.append(substr)
+            return
+
+        curr_char = self.map[digits[index]]
+        for c in curr_char:
+            self.dfs(digits, res, index + 1, substr + c)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.letterCombinations("23"))  # ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+    print(s.letterCombinations(""))  # []
+    print(s.letterCombinations("2"))  # ["a","b","c"]
