d5af008205bdb30668111e02a10e5e940eb801da
Chen Hua <chua0221@gmail.com>
2023-07-13 21:21:38 -0700

    add LC22GenerateParentheses.py

---
+++ src/main/exercise/Backtracking/LC22GenerateParentheses.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        cur = ""
+        left_count = 0
+        right_count = 0
+        self.dfs(n, res, cur, left_count, right_count)
+        return res
+
+    def dfs(self, n: int, res: List[str], cur: str, left_count: int, right_count: int) -> None:
+        if len(cur) == n*2:
+            res.append(cur)
+
+        if left_count < n:
+            self.dfs(n, res, cur + "(", left_count + 1, right_count)
+        if right_count < left_count:
+            self.dfs(n, res, cur + ")", left_count, right_count + 1)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.generateParenthesis(3))  # ["((()))","(()())","(())()","()(())","()()()"]
+    print(s.generateParenthesis(1))  # ["()"]
