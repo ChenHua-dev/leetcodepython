828da45d0879b7b8f5bfe516210b53c6ec39cf16
Chen Hua <chua0221@gmail.com>
2023-07-25 19:21:23 -0700

    add LC91DecodeWays.py

---
+++ src/main/exercise/DP/LC91DecodeWays.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s:
+            return 0
+
+        dp = []
+        for _ in range(len(s) + 1):
+            dp.append(0)
+
+        dp[0] = 1
+        if s[0] == "0":
+            dp[1] = 0
+        else:
+            dp[1] = 1
+
+        for i in range(2, len(s) + 1):
+            if 0 < int(s[i-1:i]) <= 9:
+                dp[i] += dp[i - 1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i - 2]
+                
+        return dp[len(s)]
