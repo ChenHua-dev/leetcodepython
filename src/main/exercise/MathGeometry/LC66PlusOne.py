c57adf1b546e1a4d48da96e99e2723a2acc344b6
Chen Hua <chua0221@gmail.com>
2023-08-03 19:37:11 -0700

    add LC66PlusOne.py

---
+++ src/main/exercise/MathGeometry/LC66PlusOne.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        digits.insert(0, 1)
+        return digits
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.plusOne([9,9,9]))
+    print(s.plusOne([1,9,9]))
