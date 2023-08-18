d29d038563452201394d7c852b6e747ab21c760e
Chen Hua <chua0221@gmail.com>
2023-08-17 20:02:36 -0700

    add LC56MergeInterval.py

---
+++ src/main/exercise/Intervals/LC56MergeInterval.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+
+        intervals.sort(key = lambda x:x[0])
+        merged = [intervals[0]]
+        for interval in intervals:
+            if interval[0] <= merged[-1][1]:
+                merged[-1][1] = max(interval[1], merged[-1][1])
+            else:
+                merged.append(interval)
+        return merged
